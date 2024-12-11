4a535293e7e427fd55ba7c070c047ec56f4e0bb2
Defendi <tiagodefendidasilva@gmail.com>
2024-12-11 18:07:13 -0300

    Update ex_02.cpp

--- Listas/L2 - hash/ex_02.cpp
+++ Listas/L2 - hash/ex_02.cpp
@@ -1,30 +1,32 @@
+/*
+2. Dado um vetor de inteiros arr, retorne a posição do primeiro elemento do vetor que não tem duplicata.
+Se todos os elementos ocorrerrem pelo menos duas vezes, retorne -1
+*/
+
 #include <unordered_map>
 #include <vector>
 #include <iostream>
 
 int primeiroUnico(std::vector<int>& arr) {
-    // Mapeia cada elemento ao seu numero de ocorrencias
     std::unordered_map<int, int> frequencia;
-    
-    // Primeiro loop: conta as ocorrencias de cada elemento
+
     for (int num : arr) {
         frequencia[num]++;
     }
-    
-    // Segundo loop: encontra o primeiro elemento com frequencia 1
+
     for (size_t i = 0; i < arr.size(); ++i) {
         if (frequencia[arr[i]] == 1) {
-            return i; // Retorna a posicao do primeiro elemento unico
+            return i;
         }
     }
-    
-    return -1; // Retorna -1 se nao houver elementos unicos
+
+    return -1;
 }
 
 int main() {
     std::vector<int> arr = {4, 5, 6, 5, 4, 7};
     int posicao = primeiroUnico(arr);
-    
+
     if (posicao != -1) {
         printf("A posicao do primeiro elemento unico e: %d\n", posicao);
     } else {
