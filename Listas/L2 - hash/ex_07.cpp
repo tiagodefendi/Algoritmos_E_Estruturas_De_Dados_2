30bace2fb5983abdbfd1ccee57083d0c298338c9
Defendi <tiagodefendidasilva@gmail.com>
2024-12-11 18:29:11 -0300

    Update ex_07.cpp

--- Listas/L2 - hash/ex_07.cpp
+++ Listas/L2 - hash/ex_07.cpp
@@ -1,6 +1,6 @@
 /*
 7. Dado um vetor arr não-ordenado de inteiros, retorne o comprimento da maior sequência de inteiros
-consecutivos em arr. Por exemplo, se arr = {1, 9, 3, 10, 4, 20, 2}, então a saída deve ser 4, 
+consecutivos em arr. Por exemplo, se arr = {1, 9, 3, 10, 4, 20, 2}, então a saída deve ser 4,
 pois a maior sequência de inteiros consecutivos é {1, 3, 4, 2}.
 Note que os elementos desta sequência não precisam estar um ao lado do outro no vetor arr.
 */
@@ -17,20 +17,16 @@ int maiorSequenciaConsecutivos(std::vector<int>& arr) {
     }
 
     int maiorComprimento = 0;
-    // passa pelos numeoros da terar pelos elementos do vetor
     for (int num : arr) {
-        // verificar se este numero e o inicio de uma sequencia
-        if (elementos.find(num - 1) == elementos.end()) {
+        if (!elementos.count(num - 1)) {
             int comprimentoAtual = 1;
             int atual = num;
 
-            // contar o comprimento da sequencia
-            while (elementos.find(atual + 1) != elementos.end()) {
+            while (elementos.count(atual + 1)) {
                 atual++;
                 comprimentoAtual++;
             }
 
-            // atualizar o maior comprimento encontrado
             if (maiorComprimento < comprimentoAtual) {
                 maiorComprimento = comprimentoAtual;
             }
