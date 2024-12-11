0eecd9f448950ab8bf1be97bf5d8002311619f5d
Defendi <tiagodefendidasilva@gmail.com>
2024-12-11 14:18:56 -0300

    Create ex_11.cpp

---
+++ Listas/L2 - hash/ex_11.cpp
@@ -0,0 +1,25 @@
+/*
+11. Dado um par de vetores arr1 e arr2, retorne um vetor contendo os elementos que estão em arr1 ou em
+arr2. Em outras palavras, sua função deve implementar a operação de união de conjuntos.
+*/
+
+#include <unordered_set>
+#include <vector>
+
+std::vector<int> uniao(std::vector<int>& arr1, std::vector<int>& arr2) {
+    std::unordered_set<int> h;
+    std::vector<int> result;
+
+    for(int el: arr1) {
+        h.insert(el);
+    }
+    for(int el: arr2) {
+        h.insert(el);
+    }
+
+    for(int el: h) {
+        result.push_back(el);
+    }
+
+    return result;
+}
