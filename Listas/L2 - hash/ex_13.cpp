e579a1043753ae40ca206457a6ca9190292e7dac
Defendi <tiagodefendidasilva@gmail.com>
2024-12-11 17:57:48 -0300

    Update ex_13.cpp

--- Listas/L2 - hash/ex_13.cpp
+++ Listas/L2 - hash/ex_13.cpp
@@ -8,18 +8,14 @@ não estão em arr2. Em outras palavras, sua função deve implementar a operaç
 
 std::vector<int> diferenca(std::vector<int>& arr1, std::vector<int>& arr2) {
     std::unordered_set<int> h1;
-    std::unordered_set<int> h2;
     std::vector<int> result;
 
     for(int el: arr1) {
         h1.insert(el);
     }
-    for(int el: arr2) {
-        h2.insert(el);
-    }
 
-    for(int el: h1) {
-        if (!h2.count(el)) {
+    for(int el: arr2) {
+        if (!h1.count(el)) {
             result.push_back(el);
         }
     }
